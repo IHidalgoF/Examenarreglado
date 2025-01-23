ae5658265f88dc941a8805b37e41f104daa5fcb2
IHidalgoF <ihfdez+1@gmail.com>
2025-01-23 16:25:14 +0100

    A ver si así lo arreglo

---
+++ Examen/src/isabel/hidalgo/examen/Examen.java
@@ -0,0 +1,17 @@
+package isabel.hidalgo.examen;
+
+public class Examen {
+
+	public static void main(String[] args) {
+			UtilidadesExamen.crearArray ();
+			UtilidadesExamen.mostrarArray("Muestra valores");
+			UtilidadesExamen.calculaPromedio(null);
+			UtilidadesExamen.encontrarExtremo(null);
+			UtilidadesExamen.filtrarMayoresQuePromedio(null);
+			
+				
+		
+
+	}
+
+}
---
+++ Examen/src/isabel/hidalgo/examen/UtilidadesExamen.java
@@ -0,0 +1,33 @@
+package isabel.hidalgo.examen;
+
+import java.util.Scanner;
+
+public class UtilidadesExamen {
+
+	public static void crearArray (int [] array) {
+		int tamanio =0;
+		int []numeros = new int [tamanio];
+		System.out.println("Introduce el tamaño del array");
+		Scanner scan = new Scanner (System.in);
+		
+	}	
+	public static void mostrarArray(int []array) {
+		for (int i=0; i<array.length; i++) {
+		System.out.println(i+1);			
+		}
+	}
+	
+	public static int calculaPromedio () {
+			
+	}
+	
+	public static void filtrarMayoresQuePromedio () {
+		
+	}
+		
+	
+}
+
+	
+
+
